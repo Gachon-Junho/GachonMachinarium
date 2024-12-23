8c4a362cb133ccc666bdf795095ec699dcdb4f6f
Junho <ojh0050118@gmail.com>
2024-12-24 00:35:47 +0900

    플레이어 움직임 초기 구현
    리지드 바디 떨림 문제가 있음

--- Assets/Scripts/Player/Player.cs
+++ Assets/Scripts/Player/Player.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,9 +6,12 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] 
-    private float MoveSpeed;
+    private Rigidbody rigidbody;
     
-    private float requestedPosition;
+    [SerializeField] 
+    private float moveSpeed;
+
+    private float? requestedPosition = null;
     
     void Start()
     {
@@ -17,16 +21,51 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (!Input.GetMouseButtonDown(0))
+        var mousePos = Input.mousePosition;
+        
+        if (Input.GetMouseButton(0))
+        {
+            requestedPosition = Camera.main!.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, Camera.main!.transform.position.z)).x;
+        }
+        
+        if (!requestedPosition.HasValue)
             return;
         
-        requestedPosition = Camera.main!.ScreenToWorldPoint(Input.mousePosition).x;
-
-        if (!Precision.AlmostEquals(requestedPosition, transform.position.x))
+        if (!Precision.AlmostEquals(requestedPosition.Value, transform.position.x))
         {
-            var direction = Mathf.Sign(requestedPosition - transform.position.x);
+            // 뭔가 좌표가 이상해서 요청 위치의 부호를 바꿈
+            var direction = Mathf.Sign(-requestedPosition.Value - transform.position.x);
             
-            transform.Translate(new Vector3(MoveSpeed * direction, transform.position.y, transform.position.z));
+            transform.Translate(new Vector3(moveSpeed * direction * Time.deltaTime, 0, 0));
         }
+        else
+        {
+            requestedPosition = null;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        // var mousePos = Input.mousePosition;
+        //
+        // if (Input.GetMouseButton(0))
+        // {
+        //     requestedPosition = Camera.main!.ScreenToWorldPoint(new Vector3(mousePos.x, mousePos.y, Camera.main!.transform.position.z)).x;
+        // }
+        //
+        // if (!requestedPosition.HasValue)
+        //     return;
+        //
+        // if (!Precision.AlmostEquals(requestedPosition.Value, rigidbody.position.x))
+        // {
+        //     // 뭔가 좌표가 이상해서 요청 위치의 부호를 바꿈
+        //     var direction = Mathf.Sign(-requestedPosition.Value - rigidbody.position.x);
+        //     
+        //     rigidbody.MovePosition(new Vector3(rigidbody.position.x + moveSpeed * direction * Time.fixedDeltaTime, rigidbody.position.y, rigidbody.position.z));
+        // }
+        // else
+        // {
+        //     requestedPosition = null;
+        // }
     }
 }
