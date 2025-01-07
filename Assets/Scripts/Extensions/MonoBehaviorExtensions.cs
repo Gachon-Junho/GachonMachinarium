2df982fd95516f5da5aac3203cc0bf79a7917a53
Junho <51792037+ojh050118@users.noreply.github.com>
2025-01-07 13:10:33 +0900

    `FadeTo`, `ColorTo`의 불필요한 형변환 제거

--- Assets/Scripts/Extensions/MonoBehaviorExtensions.cs
+++ Assets/Scripts/Extensions/MonoBehaviorExtensions.cs
@@ -6,7 +6,7 @@ using UnityEngine.SceneManagement;
 public static class MonoBehaviorExtensions
 {
     private static ProxyMonoBehavior proxyMonoBehavior => ProxyMonoBehavior.Current;
-    
+
     public static void LoadSceneAsync(this MonoBehaviour mono, string sceneName)
     {
         mono.StartCoroutine(loadScene());
@@ -23,7 +23,7 @@ public static class MonoBehaviorExtensions
     public static Coroutine StartDelayedCoroutine(this MonoBehaviour mono, IEnumerator routine, float timeUntilStart)
     {
         var cor = mono.StartCoroutine(startDelayed());
-        
+
         IEnumerator startDelayed()
         {
             yield return new WaitForSeconds(timeUntilStart);
@@ -37,7 +37,7 @@ public static class MonoBehaviorExtensions
     public static Coroutine StartDelayedSchedule(this MonoBehaviour mono, Action action, float timeUntilStart)
     {
         var cor = mono.StartCoroutine(startDelayed());
-        
+
         IEnumerator startDelayed()
         {
             yield return new WaitForSeconds(timeUntilStart);
@@ -74,7 +74,7 @@ public static class MonoBehaviorExtensions
             }
 
             lastVisibility = isVisible;
-            
+
             yield return null;
         }
     }
@@ -91,7 +91,7 @@ public static class MonoBehaviorExtensions
         IEnumerator transformLoop(Vector3 to, double startTime, double endTime)
         {
             var start = mono.transform.localScale;
-                
+
             while (Time.time < endTime)
             {
                 mono.transform.localScale = Interpolation.ValueAt(Time.time, start, to, startTime, endTime, new EasingFunction(easing));
@@ -102,7 +102,7 @@ public static class MonoBehaviorExtensions
 
         return cor;
     }
-    
+
     public static Coroutine MoveTo(this MonoBehaviour mono, Vector3 position, double duration = 0, Easing easing = Easing.None)
     {
         var cor = mono.StartCoroutine(transformLoop(position, Time.time, Time.time + duration));
@@ -110,7 +110,7 @@ public static class MonoBehaviorExtensions
         IEnumerator transformLoop(Vector3 to, double startTime, double endTime)
         {
             var start = mono.transform.position;
-                
+
             while (Time.time < endTime)
             {
                 mono.transform.position = Interpolation.ValueAt(Time.time, start, to, startTime, endTime, new EasingFunction(easing));
@@ -121,7 +121,7 @@ public static class MonoBehaviorExtensions
 
         return cor;
     }
-    
+
     public static Coroutine MoveToX(this MonoBehaviour mono, float x, double duration = 0, Easing easing = Easing.None)
     {
         var cor = mono.StartCoroutine(transformLoop(x, Time.time, Time.time + duration));
@@ -129,20 +129,21 @@ public static class MonoBehaviorExtensions
         IEnumerator transformLoop(float to, double startTime, double endTime)
         {
             var start = mono.transform.position.x;
-                
+
             while (Time.time < endTime)
             {
-                mono.transform.position = new Vector3(Interpolation.ValueAt(Time.time, start, to, startTime, endTime, new EasingFunction(easing)), 
+                mono.transform.position = new Vector3(
+                    Interpolation.ValueAt(Time.time, start, to, startTime, endTime, new EasingFunction(easing)),
                     mono.transform.position.y,
                     mono.transform.position.z);
-                
+
                 yield return null;
             }
         }
 
         return cor;
     }
-    
+
     public static Coroutine MoveToY(this MonoBehaviour mono, float y, double duration = 0, Easing easing = Easing.None)
     {
         var cor = mono.StartCoroutine(transformLoop(y, Time.time, Time.time + duration));
@@ -150,10 +151,10 @@ public static class MonoBehaviorExtensions
         IEnumerator transformLoop(float to, double startTime, double endTime)
         {
             var start = mono.transform.position.y;
-                
+
             while (Time.time < endTime)
             {
-                mono.transform.position = new Vector3(mono.transform.position.x, 
+                mono.transform.position = new Vector3(mono.transform.position.x,
                     Interpolation.ValueAt(Time.time, start, to, startTime, endTime, new EasingFunction(easing)),
                     mono.transform.position.z);
                 yield return null;
@@ -162,7 +163,7 @@ public static class MonoBehaviorExtensions
 
         return cor;
     }
-    
+
     public static Coroutine MoveToZ(this MonoBehaviour mono, float z, double duration = 0, Easing easing = Easing.None)
     {
         var cor = mono.StartCoroutine(transformLoop(z, Time.time, Time.time + duration));
@@ -170,10 +171,10 @@ public static class MonoBehaviorExtensions
         IEnumerator transformLoop(float to, double startTime, double endTime)
         {
             var start = mono.transform.position.z;
-                
+
             while (Time.time < endTime)
             {
-                mono.transform.position = new Vector3(mono.transform.position.x, 
+                mono.transform.position = new Vector3(mono.transform.position.x,
                                                         mono.transform.position.y,
                                                         Interpolation.ValueAt(Time.time, start, to, startTime, endTime, new EasingFunction(easing)));
 
@@ -183,22 +184,22 @@ public static class MonoBehaviorExtensions
 
         return cor;
     }
-    
-    
-    
+
+
+
     public static Coroutine RectMoveTo(this MonoBehaviour mono, Vector3 position, double duration = 0, Easing easing = Easing.None)
     {
         var transform = (mono as IHasRectTransform)?.RectTransform;
 
         if (transform == null)
             return null;
-        
+
         var cor = mono.StartCoroutine(transformLoop(position, Time.time, Time.time + duration));
 
         IEnumerator transformLoop(Vector3 to, double startTime, double endTime)
         {
             var start = transform.anchoredPosition;
-                
+
             while (Time.time < endTime)
             {
                 transform.anchoredPosition = Interpolation.ValueAt(Time.time, (Vector3)start, to, startTime, endTime, new EasingFunction(easing));
@@ -216,30 +217,30 @@ public static class MonoBehaviorExtensions
         IEnumerator transformLoop(Vector3 to, double startTime, double endTime)
         {
             var start = mono.transform.rotation.eulerAngles;
-                
+
             while (Time.time < endTime)
             {
                 mono.transform.rotation = Quaternion.Euler(Interpolation.ValueAt(Time.time, start, to, startTime, endTime, new EasingFunction(easing)));
-                
+
                 yield return null;
             }
         }
 
         return cor;
     }
-    
+
     public static Coroutine RotateTo(this Transform transform, Vector3 rotation, double duration = 0, Easing easing = Easing.None)
     {
         var cor = proxyMonoBehavior.StartCoroutine(transformLoop(rotation, Time.time, Time.time + duration));
-            
+
         IEnumerator transformLoop(Vector3 to, double startTime, double endTime)
         {
             var start = transform.rotation.eulerAngles;
-                
+
             while (Time.time < endTime)
             {
                 transform.rotation = Quaternion.Euler(Interpolation.ValueAt(Time.time, start, to, startTime, endTime, new EasingFunction(easing)));
-                
+
                 yield return null;
             }
         }
@@ -247,26 +248,20 @@ public static class MonoBehaviorExtensions
         return cor;
     }
 
-    public static Coroutine FadeTo(this MonoBehaviour mono, float alpha, double duration = 0, Easing easing = Easing.None)
+    public static Coroutine FadeTo<T>(this T mono, float alpha, double duration = 0, Easing easing = Easing.None)
+        where T : MonoBehaviour, IHasColor
     {
-        var color = (mono as IHasColor)?.Color;
-
-        if (color == null)
-            return null;
-        
         var cor = mono.StartCoroutine(transformLoop(alpha, Time.time, Time.time + duration));
 
         IEnumerator transformLoop(float to, double startTime, double endTime)
         {
-            var start = color.Value.a;
-                
+            float start = mono.Color.a;
+
             while (Time.time < endTime)
             {
-                var hasColor = (mono as IHasColor)!;
-
-                hasColor.Color = new Color(color.Value.r,
-                    color.Value.g,
-                    color.Value.b,
+                mono.Color = new Color(mono.Color.r,
+                    mono.Color.g,
+                    mono.Color.b,
                     Interpolation.ValueAt(Time.time, start, to, startTime, endTime, new EasingFunction(easing)));
 
                 yield return null;
@@ -275,23 +270,20 @@ public static class MonoBehaviorExtensions
 
         return cor;
     }
-    
-    public static Coroutine ColorTo(this MonoBehaviour mono, Color color, double duration = 0, Easing easing = Easing.None)
-    {
-        var startCol = (mono as IHasColor)?.Color;
 
-        if (startCol == null)
-            return null;
-        
+    public static Coroutine ColorTo<T>(this T mono, Color color, double duration = 0, Easing easing = Easing.None)
+        where T : MonoBehaviour, IHasColor
+    {
         var cor = mono.StartCoroutine(transformLoop(color, Time.time, Time.time + duration));
 
         IEnumerator transformLoop(Color to, double startTime, double endTime)
         {
-            var start = startCol.Value;
-                
+            var start = mono.Color;
+
             while (Time.time < endTime)
             {
-                (mono as IHasColor)!.Color = Interpolation.ValueAt(Time.time, start, to, startTime, endTime, new EasingFunction(easing));
+                mono.Color =
+                    Interpolation.ValueAt(Time.time, start, to, startTime, endTime, new EasingFunction(easing));
 
                 yield return null;
             }
