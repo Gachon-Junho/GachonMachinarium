67fd002d8aa9179fa2b607fdaddcba2961430bb8
Junho <ojh0050118@gmail.com>
2025-01-09 23:33:26 +0900

    아이템 중복 생성 차단

--- Assets/Scripts/Inventory/Item/Forest/HomeDropArea.cs
+++ Assets/Scripts/Inventory/Item/Forest/HomeDropArea.cs
@@ -9,11 +9,13 @@ public class HomeDropArea : MonoBehaviour
     [SerializeField]
     private float spawnPositionY;
 
+    private bool rewarded;
+
     private void OnTriggerEnter(Collider other)
     {
         var home = other.gameObject.GetComponent<Home>();
 
-        if (home == null)
+        if (home == null || rewarded)
             return;
 
         var item = itemInfo.CreateItem();
@@ -22,6 +24,7 @@ public class HomeDropArea : MonoBehaviour
         item.ColorTo(Color.white, 0.5f, Easing.OutQuint);
 
         Destroy(home.gameObject, 0.5f);
+        rewarded = true;
         // TODO: Home의 이미지 페이드아웃
     }
 }
