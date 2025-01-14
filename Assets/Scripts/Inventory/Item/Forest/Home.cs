498ba9b352bf3edfcd9026117b963f90c41df883
Tommy0624 <tomytomychoi@gmail.com>
2025-01-14 15:07:33 +0900

    새집 소리

--- Assets/Scripts/Inventory/Item/Forest/Home.cs
+++ Assets/Scripts/Inventory/Item/Forest/Home.cs
@@ -1,5 +1,6 @@
 using System;
 using UnityEngine;
+using UnityEngine.XR;
 
 public class Home : MonoBehaviour
 {
@@ -9,6 +10,9 @@ public class Home : MonoBehaviour
     [SerializeField]
     private int countUntilDrop = 2;
 
+    [SerializeField]
+    private AudioClip fall;
+
     // TODO: 이미지 들어오면 IHasColor 추가
     private void OnCollisionEnter(Collision other)
     {
@@ -17,6 +21,7 @@ public class Home : MonoBehaviour
             if (--countUntilDrop <= 0)
             {
                 // 2번 맞음
+                ProxyMonoBehavior.Current.Play(fall);
                 rigidbody.isKinematic = false;
 
                 return;
