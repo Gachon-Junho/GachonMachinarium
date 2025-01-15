22552d695aa71ab30e320db56e0338761f7d9420
Junho <51792037+ojh050118@users.noreply.github.com>
2025-01-15 10:48:20 +0900

    최초 영상 재생완료 이후 재생 차단

---
+++ Assets/Scripts/VideoController.cs
@@ -0,0 +1,41 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Video;
+
+public class VideoController : MonoBehaviour, IHasColor
+{
+    public Color Color
+    {
+        get => image.color;
+        set => image.color = value;
+    }
+
+    [SerializeField]
+    private VideoPlayer videoPlayer;
+
+    [SerializeField]
+    private RawImage image;
+
+    private bool played;
+
+    private void Start()
+    {
+        if (played)
+            return;
+
+        this.ColorTo(Color.white, 0.1f, Easing.Out);
+
+        videoPlayer.Play();
+        played = true;
+
+        this.StartDelayedSchedule(() =>
+        {
+            this.ColorTo(Color.clear, 1, Easing.Out);
+            this.StartDelayedSchedule(() =>
+            {
+                gameObject.SetActive(false);
+            }, 1);
+        }, (float)videoPlayer.clip.length);
+    }
+}
